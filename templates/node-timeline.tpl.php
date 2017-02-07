61ab735cc60c5f93bb16d9dfdc5cc33fc0aec8b9
Nick Mathew <nim8@georgetown.edu>
2017-02-07 10:29:46 +0300

    Initial commit of Timeline integration

---
+++ templates/node-timeline.tpl.php
@@ -0,0 +1,20 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php if ($title_prefix || $title_suffix || $display_submitted || !$status || !$page && $title): ?>
+    <header>
+      <?php print render($title_prefix); ?>
+      <?php if (!$page && $title): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($display_submitted): ?>
+        <p class="submitted">
+          <?php print $user_picture; ?>
+          <?php print $submitted; ?>
+        </p>
+      <?php endif; ?>
+    </header>
+  <?php endif; ?>
+
+</article>
